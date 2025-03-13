2219dc9d1a371ca546e1126cf52854233f785d6b
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2025-03-13 17:44:01 +0200

    minor

--- experiment/src/main.py
+++ experiment/src/main.py
@@ -1,7 +1,7 @@
 import kobe2
 
 def main():
-    kobe2.kobe(input='benchmarking/configs', output='benchmarking/experiments')
+    kobe2.kobe(input='configs', output='experiments')
 
 if __name__ == "__main__":
     main()
