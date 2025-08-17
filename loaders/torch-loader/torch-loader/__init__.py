9f4e45dc40999c6354a1ecf0e503453117380aed
Natalia Koliou <natalykoliou@yahoo.gr>
2025-08-17 19:37:22 +0300

    Add main.py to torch loader

--- loaders/torch-loader/torch-loader/__init__.py
+++ loaders/torch-loader/torch-loader/__init__.py
@@ -1,23 +1,3 @@
-from .shared import *
-from .tabular import *
-from .utils import *
+from .main import main as preprocess
 
-__all__ = [
-    "split_data",
-    "extract_weights",
-    "shift_labels",
-    "create_dataloader",
-    "get_logger",
-    "get_dir",
-    "get_path",
-    "save_npz",
-    "load_npz",
-    "save_npy",
-    "load_npy",
-    "save_json",
-    "load_json",
-    "standard_normalize",
-    "robust_normalize",
-    "get_stats",
-    "TSDataset",
-]
+__all__ = ["preprocess"]
---
+++ loaders/torch-loader/torch-loader/main.py
@@ -0,0 +1,63 @@
+import os
+from . import tabular as tl
+
+logger = tl.get_logger(level='INFO')
+
+def main(dir, name, process, batch_size, train_size, val_size, test_size, seq_len, norm_include, full_epoch, per_epoch):
+    """
+    Main function to create torch loaders from the Bitbrain dataset, suitable for machine learning tasks.
+    """
+    dls = {'train': None, 'val': None, 'test': None}
+
+    process_map = {"prepare": ["train", "val"],
+                   "work": ["test"]}
+
+    logger.info("Shifting labels in the entire dataset.")
+    tl.shift_labels(dir, name=name)
+
+    logger.info("Splitting data into train, val, test.")
+    tl.split_data(dir=dir, 
+                  name=name, 
+                  train_size=train_size, 
+                  val_size=val_size, 
+                  test_size=test_size)
+    
+    weights = tl.extract_weights(dir, name=name)
+    logger.info(f"Training data class weights:\n{weights}")
+
+    stats = tl.get_stats(dir, name=name)
+    logger.info(f"Calculated statistics from training data.")
+
+    for p in process_map.get(process, []):
+        logger.info(f"Normalizing {p} data with standard normalization.")
+        tl.standard_normalize(dir=dir,
+                              name=name,
+                              process=p,
+                              include=norm_include,
+                              stats=stats)
+        
+        logger.info(f"Normalizing {p} data with robust normalization.")
+        tl.robust_normalize(dir=dir,
+                            name=name,
+                            process=p,
+                            include=norm_include,
+                            stats=stats)
+        
+        logger.info(f"Creating TSDataset for {p} data.")
+        ds = tl.TSDataset(dir=dir, 
+                          name=f'{name}-{p}-rbst-norm',
+                          seq_len=seq_len,
+                          full_epoch=full_epoch,
+                          per_epoch=per_epoch)
+        
+        logger.info(f"Creating dataloader for {p} data.")
+        dls[p] = tl.create_dataloader(ds=ds, 
+                                      batch_size=batch_size, 
+                                      shuffle=[True, False, False], 
+                                      num_workers=None, 
+                                      drop_last=False)
+    
+    return dls
+
+if __name__ == "__main__":
+    main()
