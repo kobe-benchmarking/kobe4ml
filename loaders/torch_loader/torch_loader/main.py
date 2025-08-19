fd9ff17900c12b289b721e604ebd3a920de6d777
Natalia Koliou <natalykoliou@yahoo.gr>
2025-08-19 19:02:41 +0300

    Fix references to modules in torch loader

--- loaders/torch_loader/torch_loader/main.py
+++ loaders/torch_loader/torch_loader/main.py
@@ -1,6 +1,7 @@
 import os
 from . import utils
 from . import tabular as tl
+from . import shared as sh
 
 logger = utils.get_logger(level='INFO')
 
@@ -14,16 +15,16 @@ def main(dir, name, process, batch_size, train_size, val_size, test_size, seq_le
                    "work": ["test"]}
 
     logger.info("Shifting labels in the entire dataset.")
-    tl.shift_labels(dir, name=name)
+    sh.shift_labels(dir, name=name)
 
     logger.info("Splitting data into train, val, test.")
-    tl.split_data(dir=dir, 
+    sh.split_data(dir=dir, 
                   name=name, 
                   train_size=train_size, 
                   val_size=val_size, 
                   test_size=test_size)
     
-    weights = tl.extract_weights(dir, name=name)
+    weights = sh.extract_weights(dir, name=name)
     logger.info(f"Training data class weights:\n{weights}")
 
     stats = tl.get_stats(dir, name=name)
@@ -52,7 +53,7 @@ def main(dir, name, process, batch_size, train_size, val_size, test_size, seq_le
                           per_epoch=per_epoch)
         
         logger.info(f"Creating dataloader for {p} data.")
-        dls[p] = tl.create_dataloader(ds=ds, 
+        dls[p] = sh.create_dataloader(ds=ds, 
                                       batch_size=batch_size, 
                                       shuffle=[True, False, False], 
                                       num_workers=None, 
