0c60fc93dbc2b9f960ec5c23a07d675123616238
Natalia Koliou <natalykoliou@yahoo.gr>
2025-09-22 17:42:29 +0300

    Temporary commit until bug fix

--- loaders/sklearn_loader/sklearn_loader/shared.py
+++ loaders/sklearn_loader/sklearn_loader/shared.py
@@ -132,15 +132,15 @@ def sort_data(dir, name, process, done):
     data = utils.load_npz(data_path)
     metadata = utils.load_json(meta_path)
 
-    all_data = data['columns']
+    sort_data = data['sort']
     sort_cols = metadata['sort']
 
     if not sort_cols or done:
         logger.info(f"No sort columns specified for {name}, skipping sorting.")
         return
 
-    sort_indices = np.lexsort([all_data[col] for col in reversed(sort_cols)])
-    sorted_data = {k: v[sort_indices] for k, v in all_data.items()}
+    sort_indices = np.lexsort([sort_data[col] for col in reversed(sort_cols)])
+    sorted_data = {k: v[sort_indices] for k, v in data.items()}
 
     utils.save_npz(sorted_data, data_path)
     logger.info(f"Sorted dataset '{name}-{process}' by {sort_cols} and saved to {data_path}.")
